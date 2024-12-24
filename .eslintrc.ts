a0213b3dc92b1901068ba6795cc4de2ee65e4a51
KooTaehyeon <vcro12123@naver.com>
2024-12-24 18:06:28 +0900

    chore: add ESLint and Prettier configuration

---
+++ .eslintrc.ts
@@ -0,0 +1,39 @@
+const config = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-native', 'prettier'] as any, // 타입 강제 우회 필요 시 `as any` 추가 가능
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    'react-native/react-native': true,
+  },
+};
+
+export default config;

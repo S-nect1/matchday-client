b4b693b2eaa6c6bdcbc93f41c63512cd36c8ee0b
itsmesunky <itisp_06@naver.com>
2025-01-03 11:55:02 +0900

    refactor: remove import

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { SafeAreaView } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { Theme } from './src/styles/theme';
+import Input from './src/components/@common/Input';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,6 +19,7 @@ export default function App() {
     <ThemeProvider theme={Theme}>
       <SafeAreaView>
         <StatusBar style="auto" />
+        <Input />
       </SafeAreaView>
     </ThemeProvider>
   );
